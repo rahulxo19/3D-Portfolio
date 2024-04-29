94802153759daacda9075ead84614a4b72b18a1e
rahulxo19 <rahulchhikara2001@gmail.com>
2024-04-29 05:24:49 -0400

    testing portfolio

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://rahulxo19.github.io/3D-Portfolio/",
 })
