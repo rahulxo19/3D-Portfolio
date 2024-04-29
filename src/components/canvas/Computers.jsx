6ea8ecd4abce6d61767d3073eec7ba2d27e69e52
rahulxo19 <rahulchhikara2001@gmail.com>
2024-04-29 03:32:51 -0400

    testing portfolio

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,6 +5,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
+  // importing the model
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
